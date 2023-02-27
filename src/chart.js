5e4f023e815effca961ab4fa8427ae8f037ec794
gerardsg94 <gerard_sanchez94@uoc.edu>
2023-02-27 20:40:45 +0100

    first commit

---
+++ src/chart.js
@@ -0,0 +1,43 @@
+google.charts.load('current', {packages: ['corechart', 'bar']});
+google.charts.setOnLoadCallback(drawBasic);
+
+function drawBasic() {
+
+    var data = google.visualization.arrayToDataTable([
+        ["Piezas", "Numero", { role: "style" } ],
+        ["Paros", 10.49, "red"],
+        ["Preparada", 19.30, "orange"],
+        ["Producida", 21.45, "green"]
+    ]);
+
+    var view = new google.visualization.DataView(data);
+    view.setColumns([0, 1,
+                     { calc: "stringify",
+                       sourceColumn: 1,
+                       type: "string",
+                       role: "annotation" },
+                     2]);
+
+      var options = {
+        width: 300,
+        height: "auto",
+        backgroundColor:'#EEEDED',
+        legend: { position: "none"},
+        chartArea:{
+            baseline:2 
+        },
+        hAxis: {
+          minValue: 0
+        },
+        vAxis: {
+          title: 'KPI',
+
+        },
+      };
+
+      var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
+
+      
+
+      chart.draw(view, options);
+    }
